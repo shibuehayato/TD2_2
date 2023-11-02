a084a3d4636e3322840d111adeeccd113d402c5c
shibuehayato <115055046+shibuehayato@users.noreply.github.com>
2023-11-02 18:20:54 +0900

    フォルダ階層修正

---
+++ DirectXGame/Player/Player.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include "Model.h"
+#include "WorldTransform.h"
+#include "Input.h"
+
+class Player {
+public:
+	void Initialize(Model* head, Model* body, Model* L_arm, Model* R_arm);
+
+	void Update();
+
+	void Draw(ViewProjection viewProjection);
+
+	const WorldTransform& GetWorldTransform() { return worldTransformBody_; }
+
+	void SetViewProjection(const ViewProjection* viewProjection)
+	{
+		viewProjection_ = viewProjection;
+	}
+
+	// 浮遊ギミック初期化
+	void InitializeFloatingGimmick();
+	// 浮遊ギミック更新
+	void UpdateFloatingGimmick();
+
+private:
+	// ワールド変換データ
+	WorldTransform worldTransformHead_;
+	WorldTransform worldTransformBody_;
+	WorldTransform worldTransformL_arm_;
+	WorldTransform worldTransformR_arm_;
+	// モデル
+	Model* modelFighterHead_ = nullptr;
+	Model* modelFighterBody_ = nullptr;
+	Model* modelFighterL_arm_ = nullptr;
+	Model* modelFighterR_arm_ = nullptr;
+
+	// キーボード入力
+	Input* input_ = nullptr;
+
+	// カメラのビュープロジェクション
+	const ViewProjection* viewProjection_ = nullptr;
+
+	// 浮遊ギミックの媒介変数
+	float floatingParameter_ = 0.0f;
+
+	// 浮遊の振幅<m>
+	float range_ = 0.4f;
+
+	float head_[3] = {0, 1.8f, 0};
+	float body_[3] = {0, 0.2f, 0};
+	float l_arm[3] = {-0.6f, 1.5f, 0};
+	float r_arm[3] = {0.6f, 1.5f, 0};
+};
---
+++ DirectXGame/ground/Ground.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "Model.h"
+#include "WorldTransform.h"
+
+class Ground {
+public:
+	void Initialize(Model* model);
+
+	void Update();
+
+	void Draw(ViewProjection viewProjection);
+
+private:
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+	// モデル
+	Model* model_ = nullptr;
+};
