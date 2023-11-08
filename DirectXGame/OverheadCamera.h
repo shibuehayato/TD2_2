ff67c89c9103125df2f57cc0fb857396f1de0bdb
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-11-08 16:47:08 +0900

    Towerの生成ができた。

--- DirectXGame/OverheadCamera.h
+++ DirectXGame/OverheadCamera.h
@@ -1,15 +1,2 @@
 #pragma once
-#include"WorldTransform.h"
-#include"ViewProjection.h"
 
-class OverheadCamera {
-public:
-	void Initialize();
-
-	void Update();
-
-	private:
-	WorldTransform worldTransform_;
-
-	ViewProjection viewProjection_;
-};
