d9f10be0848e8fa31ef6073932a72c8def6fc73f
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-11-29 12:13:34 +0900

    UIの画像を追加した。

--- DirectXGame/UI.cpp
+++ DirectXGame/UI.cpp
@@ -1,26 +1,13 @@
 #include "UI.h"
 
 void UI::Initialize() {
-	texturesUI[0] = TextureManager::Load("Sample.png");
-	texturesUI[1] = TextureManager::Load("uvChecker.png");
-	texturesUI[2] = TextureManager::Load("uvChecker.png");
-	texturesUI[3] = TextureManager::Load("debugfont.png");
-	texturesUI[4] = TextureManager::Load("debugfont.png");
-	texturesUI[5] = TextureManager::Load("debugfont.png");
+	texturesUI[0] = TextureManager::Load("UI.png");
+
 
 
 	uisprites_[0] =
-	    Sprite::Create(texturesUI[0], {1000.0f, 700.0f}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.5f, 0.5f});
-	uisprites_[1] =
-	    Sprite::Create(texturesUI[1], {500.0f, 700.0f}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.5f, 0.5f});
-	uisprites_[2] =
-	    Sprite::Create(texturesUI[2], {1200.0f, 700.0f}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.5f, 0.5f});
-	uisprites_[3] =
-	    Sprite::Create(texturesUI[3], {950.0f, 700.0f}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.5f, 0.5f});
-	uisprites_[4] =
-	    Sprite::Create(texturesUI[4], {450.0f, 700.0f}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.5f, 0.5f});
-	uisprites_[5] =
-	    Sprite::Create(texturesUI[5], {1150.0f, 700.0f}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.5f, 0.5f});
+	    Sprite::Create(texturesUI[0], {600.0f,350.0f}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.5f, 0.5f});
+	
 
 }
 
@@ -28,10 +15,6 @@ void UI::Update() {}
 
 void UI::Draw() { 
 	uisprites_[0]->Draw(); 
-	uisprites_[1]->Draw();
-	uisprites_[2]->Draw();
-	uisprites_[3]->Draw();
-	uisprites_[4]->Draw();
-	uisprites_[5]->Draw();
+	
 
 }
